2e97d8b3fbc27281bbb2406f44a576c1838448ca
hyos <hyozzang99@gmail.com>
2025-12-14 20:43:50 +0900

    Refactor: 프론트 컨트롤러가 모델을 생성하고 컨트롤러는 뷰 이름만 반환하도록 변경

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -15,33 +15,31 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "front-controller/v4/*")
 public class FrontControllerServletV4 extends HttpServlet {
 
     private Map<String, ControllerV4> controllerMap = new HashMap<>();
 
     public FrontControllerServletV4() {
-        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
-        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
-        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+        controllerMap.put("front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("front-controller/v4/members", new MemberListControllerV4());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         String requestURI = request.getRequestURI();
         ControllerV4 controller = controllerMap.get(requestURI);
         if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
-
-        //paramMap
+        // 요청 데이터를 뽑아서 어떤 뷰에 어떤 데이터를 넣을지
         Map<String, String> paramMap = createParamMap(request);
-        Map<String, Object> model = new HashMap<>(); //추가된 코드
+        Map<String, Object> model = new HashMap<>();
+
         String viewName = controller.process(paramMap, model);
 
-        //new-form, save, list
         MyView view = viewResolver(viewName);
         view.render(model, request, response);
     }
